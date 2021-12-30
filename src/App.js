e15137720421d253cc6f90943db8a61476e98c5b
Karin Portillo <karin.portillo@infor.com>
2021-12-30 19:41:41 +0100

    Added shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      // <div className='App'>
+      //   <HomePage />
+      // </div>
+      <div>
+        <Routes>        
+          <Route excact path="/" element={<HomePage/>} />
+          <Route path="/shop" element={<ShopPage/>} />
+        </Routes>
+
       </div>
     );
   }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./menu-item.styles.scss"
 
-const MenuItem = ({title, imageUrl,size}) => (
-    <div className={ `${size} menu-item`}>
-    
-    <div className="background-image" style={{
-        backgroundImage: `url(${imageUrl})`
-    }}></div>
-    <div className='content'>
-      <div className='title'>{title.toUpperCase()}</div>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-);
+// const MenuItem = ({title, imageUrl,size}) => (
+//     <div className={ `${size} menu-item`}>
 
-export default MenuItem;
+//     <div className="background-image" style={{
+//         backgroundImage: `url(${imageUrl})`
+//     }}></div>
+//     <div className='content'>
+//       <div className='title'>{title.toUpperCase()}</div>
+//       <span className='subtitle'>SHOP NOW</span>
+//     </div>
+//   </div>
+// );
+
+// export default withRouter(MenuItem);
+
+export default function MenuItem({ title, imageUrl, size, linkUrl }) {
+
+  let navigate = useNavigate();
+  
+  function handleClick() {
+    // TODO add relative current URL
+    navigate(linkUrl);
+  }
+
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+
+      <div className="background-image" style={{
+        backgroundImage: `url(${imageUrl})`
+      }}></div>
+      <div className='content'>
+        <div className='title'>{title.toUpperCase()}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>);
+}
